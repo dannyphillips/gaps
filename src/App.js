f36803baef11b31a121b1342d08d7cba5edf8b3a
Danny Phillips <danny.phillips8@gmail.com>
2019-06-27 23:09:17 -0700

    v1 Gaps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,148 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Layout, Input, Button } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// We import our firestore module
+import firestore from "./firestore";
+import List from "./List";
+
+import "./App.css";
+
+const { Header, Footer, Content } = Layout;
+
+const COLLECTION_NAME = "gaps";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addingItem: false,
+      pendingItem: "",
+      data: {
+        todo: [],
+        completed: [],
+        archived: []
+      }
+    };
+    this.addItem = this.addItem.bind(this);
+    this.toggleComplete = this.toggleComplete.bind(this);
+    firestore.collection(COLLECTION_NAME).onSnapshot(snapshot => {
+      let todo = [];
+      let archived = [];
+      let completed = [];
+      snapshot.forEach(doc => {
+        const item = doc.data();
+        item.id = doc.id;
+        if (item.archived) archived.push(item);
+        else if (item.completed) completed.push(item);
+        else todo.push(item);
+      });
+      todo.sort(function(a, b) {
+        return (
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+        );
+      });
+      archived.sort(function(a, b) {
+        return (
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+        );
+      });
+      completed.sort(function(a, b) {
+        return (
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+        );
+      });
+      this.setState({ data: {todo, completed, archived }});
+    });
+  }
+
+  async toggleComplete(item) {
+    await firestore
+      .collection(COLLECTION_NAME)
+      .doc(item.id)
+      .update({
+        completed: !item.completed,
+        completedAt: new Date().toISOString()
+      });
+  }
+  async toggleArchive(item) {
+    await firestore
+      .collection(COLLECTION_NAME)
+      .doc(item.id)
+      .update({
+        archived: !item.archived,
+        archivedAt: new Date().toISOString()
+      });
+  }
+  async deleteItem(item) {
+    await firestore
+      .collection(COLLECTION_NAME)
+      .doc(item.id)
+      .delete();
+  }
+
+  async addItem() {
+    if (!this.state.pendingItem) return;
+    this.setState({ addingItem: true });
+    await firestore.collection(COLLECTION_NAME).add({
+      content: this.state.pendingItem,
+      completed: false,
+      archived: false,
+      createdAt: new Date().toISOString()
+    });
+    this.setState({ addingItem: false, pendingItem: "" });
+  }
+
+  render() {
+    const cb = {
+      complete: this.toggleComplete,
+      archive: this.toggleArchive,
+      delete: this.deleteItem
+    };
+    return (
+      <Layout className="App">
+        <Header className="App-header">
+          <h1>Gaps</h1>
+        </Header>
+        <Content className="App-content">
+          <Input
+            ref="add-todo-input"
+            className="App-add-todo-input"
+            size="large"
+            placeholder="What don't you know?"
+            disabled={this.state.addingItem}
+            onChange={evt => this.setState({ pendingItem: evt.target.value })}
+            value={this.state.pendingItem}
+            onPressEnter={this.addItem}
+            required
+          />
+          <Button
+            className="App-add-todo-button"
+            size="large"
+            type="primary"
+            onClick={this.addItem}
+            loading={this.state.addingItem}
+          >
+            Add Item
+          </Button>
+          <br />
+          <br />
+          <List title="Todo" type="todo" items={this.state.data.todo} cb={cb} />
+          <List
+            title="Completed"
+            type="completed"
+            items={this.state.data.completed}
+            cb={cb}
+          />
+          <List
+            title="Archived"
+            type="archived"
+            items={this.state.data.archived}
+            cb={cb}
+          />
+        </Content>
+        <Footer className="App-footer">&copy; Danny Phillips. 2019</Footer>
+      </Layout>
+    );
+  }
 }
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { List, Icon } from "antd";
+
+const actions = {
+  todo: ['complete','archive'],
+  completed: ['complete', 'archive'],
+  archived: ['archive', 'delete']
+}
+const iconMap = {
+  complete: 'check',
+  archive: 'inbox',
+  delete: 'close-circle'
+}
+const GapList = ({ title, items, type, cb }) => (
+  <div>
+    {title}
+    <List
+      className="App-todos"
+      size="large"
+      bordered
+      dataSource={items}
+      renderItem={todo => (
+        <List.Item className="App-todo">
+          {todo.content}
+          <div>
+            {actions[type].map((action) => {
+              return <Icon
+                key={action}
+                onClick={evt => cb[action](todo)}
+                className="App-todo-icon"
+                type={iconMap[action]}
+              />;
+            })}
+          </div>
+        </List.Item>
+      )}
+    />
+  </div>
+);
+
+export default GapList;
